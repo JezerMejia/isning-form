02a68987672273b32df93bba1eba0bf9861bcbeb
Jezer Mejía <amyuki4@icloud.com>
2025-02-25 15:22:47 -0600

    Remove geist font usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Isning Form",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <div
           className={
             "mb-20 sm:mb-16 py-8 lg:py-12 flex flex-col gap-24 container mx-auto 2xl:max-w-7xl px-3"
